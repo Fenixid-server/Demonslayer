ae7c48ec7b92fae579f48093166878da0895d386
Fenixid-server/ <Privateloginemails@gmail.com>
2025-02-12 18:11:15 +0530

    new project

---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    autoLikeStatus: true,
+    downloadMediaStatus: false,
+    sensorNumber: true,
+    blackList: [],
+    whiteList: [],
+    ownerNumber: '94764921555',
+    botDetails: {
+        botName: 'FENIX ID OWNER',
+        botAge: '25',
+        botLocation: 'COLOMBO'
+    }
+};
